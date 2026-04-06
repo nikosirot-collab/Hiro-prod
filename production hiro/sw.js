f7053098308310d2ad081468d4d9d47f0ae48ae7
nikosirot-collab <niko.sirot@gmail.com>
2026-04-06 14:22:16 +1100

    Add files via upload

---
+++ production hiro/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'hiro-v1';
+const ASSETS = [
+  'https://nikosirot-collab.github.io/Hiro-prod/production%20hiro/hiro_prod.html',
+  'https://nikosirot-collab.github.io/Hiro-prod/production%20hiro/hiro_access.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.url.includes('firestore.googleapis.com') ||
+     e.request.url.includes('firebase')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
